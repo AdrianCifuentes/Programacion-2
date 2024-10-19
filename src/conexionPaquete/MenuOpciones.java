3ceb1bd9e57f73c246b2f2534abf048a80818387
Adrian Cifuentes <Adrian Cifuentes@Adrin>
2024-10-19 15:21:01 -0600

    Updated

---
+++ src/conexionPaquete/MenuOpciones.java
@@ -0,0 +1,28 @@
+package conexionPaquete;
+import java.util.Scanner;
+
+public class MenuOpciones {
+     public int opcionMenu;
+   Scanner scanner = new Scanner(System.in);
+    /*public static void menuOpt(){
+    int opcionMenu;
+    System.out.println("*******************************");
+    System.out.println("**  Ingrese un número para   **");
+    System.out.println("**  seleccionar una opción   **");
+    System.out.println("**  del munú principal...    **");
+    System.out.println("*******************************");
+    System.out.println("*******************************");
+    System.out.println("****   MENU PRINCIPAL   *******");
+    System.out.println("** (1)...Ingresar Porducto   **");
+    System.out.println("** (2)...Mostrar Porducto    **");
+    System.out.println("** (3)...Buscar Porducto     **");
+    System.out.println("** (4)...Modificar Porducto  **");
+    System.out.println("** (5)...Eliminar Porducto   **");
+    System.out.println("** (5)...Salir del menú      **");
+    System.out.println("*******************************");
+    // Leer el valor entero ingresado por el usuario
+    int opcionMenu = scanner.nextInt();
+ 
+}//Fin de menu
+    */  
+}
